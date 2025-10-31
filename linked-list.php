ace7b0a8fa6230719e1b19b34555457b7191196f
unknown <your.email@example.com>
2025-10-31 20:31:31 +0800

    ex1: Linked-list

--- linked-list.php
+++ linked-list.php
@@ -1,3 +1,32 @@
-<?php
 
-echo "hello world";
+<?php
+class Node{
+    public $data;
+    public $next;
+
+    public function __construct($data){
+        $this->data = $data;
+        $this->next = null;
+    }
+    
+    public function display(){
+        $current = $this;
+
+        while($current !== null){
+            $current = $current;
+            echo $current->data . ' -> ';
+            $current = $current->next;
+        }
+        echo "NULL\n";
+    }
+}
+
+$node1 = new Node(10);
+$node2 = new Node(20);
+$node3 = new Node(30);
+
+$node1->next = $node2;
+$node2->next = $node3;
+
+$node1->display();
+
